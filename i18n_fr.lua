36b1f17fc97cb5f3de29e8379b3c495c8da72c24
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,6 @@
 return {
     ["%1\xC3\x97%2 \xC2\xB7 %3 \xC2\xB7 Empty: %4%5%6"] = { fr = "%1×%2 · %3 · Vides : %4%5%6" },
     [" \xC2\xB7 Note ON"]              = { fr = " · Notes ON" },
+    ["Fill the grid with digits satisfying row, column and cage arithmetic constraints."] = { fr = "Remplissez la grille de chiffres respectant les contraintes arithmétiques de ligne, de colonne et de cage." },
+    ["KenKen"]                         = { fr = "KenKen" },
 }
